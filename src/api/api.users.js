fc60c67aec243deec6b4dafc45e4925312a14162
Ilya1910 <targaev94@gmail.com>
2021-10-09 18:11:20 +0300

    badge module

--- src/api/api.users.js
+++ src/api/api.users.js
@@ -1,84 +1,102 @@
+const role = {
+    teamLead: {
+        _id: "1",
+        name: "Тимлид",
+        color: "danger",
+    },
+    frontend: {
+        _id: "2",
+        name: "Front-end разработчик",
+        color: "primary",
+    },
+};
+
 const users = [
-	{
-		_id: '1',
-		firstName: 'Daniiel',
-		lastName: 'Kondratiuk',
-		about: 'Lorem ipsum',
-		age: 26,
-		avatar: 'https://picsum.photos/250/200',
-		socials: [
-			{
-				name: 'GitHub',
-				link: '',
-			}
-		],
-		skills: ['HTML','CSS','JavaScript','ReactJS','NodeJS'],
-	},
-	{
-		_id: '2',
-		firstName: 'Natalia',
-		lastName: 'Schermann',
-		about: 'Lorem ipsum',
-		age: 16,
-		avatar: 'https://picsum.photos/250/200',
-		socials: [
-			{
-				name: 'GitHub',
-				link: '',
-			}
-		],
-		skills: ['HTML','CSS','JavaScript','ReactJS','NodeJS'],
-	},
-	{
-		_id: '3',
-		firstName: 'Denis',
-		lastName: 'Monet',
-		about: 'Lorem ipsum',
-		age: 36,
-		avatar: 'https://picsum.photos/250/200',
-		socials: [
-			{
-				name: 'GitHub',
-				link: '',
-			}
-		],
-		skills: ['HTML','CSS','JavaScript','ReactJS','NodeJS'],
-	},
-	{
-		_id: '4',
-		firstName: 'Alex',
-		lastName: 'Defo',
-		about: 'Lorem ipsum',
-		age: 69,
-		avatar: 'https://picsum.photos/250/200',
-		socials: [
-			{
-				name: 'GitHub',
-				link: '',
-			}
-		],
-		skills: ['HTML','CSS','JavaScript','ReactJS','NodeJS'],
-	},
-	{
-		_id: '5',
-		firstName: 'Ilya',
-		lastName: 'Mendeleev',
-		about: 'Lorem ipsum',
-		age: 43,
-		avatar: 'https://picsum.photos/250/200',
-		socials: [
-			{
-				name: 'GitHub',
-				link: '',
-			}
-		],
-		skills: ['HTML','CSS','JavaScript','ReactJS','NodeJS'],
-	},
-]
+    {
+        _id: "1",
+        firstName: "Daniiel",
+        lastName: "Kondratiuk",
+        about: "Lorem ipsum",
+        age: 26,
+        role: role.teamLead,
+        avatar: "https://picsum.photos/250/200",
+        socials: [
+            {
+                name: "GitHub",
+                link: "",
+            },
+        ],
+        skills: ["HTML", "CSS", "JavaScript", "ReactJS", "NodeJS"],
+    },
+    {
+        _id: "2",
+        firstName: "Natalia",
+        lastName: "Schermann",
+        about: "Lorem ipsum",
+        age: 16,
+        role: role.frontend,
+        avatar: "https://picsum.photos/250/200",
+        socials: [
+            {
+                name: "GitHub",
+                link: "",
+            },
+        ],
+        skills: ["HTML", "CSS", "JavaScript", "ReactJS", "NodeJS"],
+    },
+    {
+        _id: "3",
+        firstName: "Denis",
+        lastName: "Monet",
+        about: "Lorem ipsum",
+        age: 36,
+        role: role.frontend,
+        avatar: "https://picsum.photos/250/200",
+        socials: [
+            {
+                name: "GitHub",
+                link: "",
+            },
+        ],
+        skills: ["HTML", "CSS", "JavaScript", "ReactJS", "NodeJS"],
+    },
+    {
+        _id: "4",
+        firstName: "Alex",
+        lastName: "Defo",
+        about: "Lorem ipsum",
+        age: 69,
+        role: role.frontend,
+        avatar: "https://picsum.photos/250/200",
+        socials: [
+            {
+                name: "GitHub",
+                link: "",
+            },
+        ],
+        skills: ["HTML", "CSS", "JavaScript", "ReactJS", "NodeJS"],
+    },
+    {
+        _id: "5",
+        firstName: "Ilya",
+        lastName: "Targaev",
+        about: "Lorem ipsum",
+        age: 26,
+        role: role.frontend,
+        avatar: "https://picsum.photos/250/200",
+        socials: [
+            {
+                name: "GitHub",
+                link: "",
+            },
+        ],
+        skills: ["HTML", "CSS", "JavaScript", "ReactJS", "NodeJS"],
+    },
+];
 
 const fetchAll = () =>
-	new Promise((resolve) => {
-		setTimeout(() => resolve(users), 300)
-	})
+    new Promise((resolve) => {
+        setTimeout(() => resolve(users), 300);
+    });
 
-export default fetchAll
+export default fetchAll;
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Badge from "./badge";
 
-const User = ({users, match}) => {
-	const userId = match.params.userId
-	const getUsersById = (id) => users.find(user=>user._id===id)
-	return <h1>{getUsersById(userId)?.firstName}</h1>
-}
+const User = ({ users, match }) => {
+    const userId = match.params.userId;
+    const getUsersById = (id) => users.find((user) => user._id === id);
+    const role = getUsersById(userId).role;
+    return (
+        <>
+            <h1>{getUsersById(userId)?.firstName}</h1>
+            <Badge role={role} />
+        </>
+    );
+};
 
-export default User
+export default User;
