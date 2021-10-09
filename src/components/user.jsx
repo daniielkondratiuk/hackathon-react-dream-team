b0db4c70326159459bba5c9e46d21275e0ceedf3
Ilya1910 <targaev94@gmail.com>
2021-10-09 18:03:57 +0300

    Revert "Badge  module"
    This reverts commit e644e72c5c785284033dc6cdb79bd2f239a29369.

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,16 +1,9 @@
-import React from "react";
-import Badge from "./badge";
+import React from 'react'
 
-const User = ({ users, match }) => {
-    const userId = match.params.userId;
-    const getUsersById = (id) => users.find((user) => user._id === id);
-    const role = getUsersById(userId).role;
-    return (
-        <>
-            <h1>{getUsersById(userId)?.firstName}</h1>
-            <Badge role={role} />
-        </>
-    );
-};
+const User = ({users, match}) => {
+	const userId = match.params.userId
+	const getUsersById = (id) => users.find(user=>user._id===id)
+	return <h1>{getUsersById(userId)?.firstName}</h1>
+}
 
-export default User;
+export default User
